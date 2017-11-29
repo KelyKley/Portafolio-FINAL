6ef2635444b934ea84b3ed67e7a29e8baf229130
KelyKley <kelyanamurohuamani@gmail.com>
2017-11-28 22:09:51 -0500

    modificando el orden de vistas, mas insercion de logo para redireccionar al home, falta los skills y los proyectos, contancto y aboutme hecho

--- src/about.js
+++ src/about.js
@@ -2,28 +2,39 @@ import React, { Component } from "react";
 import { connect } from "redux-zero/react";
 import "./css/app.css";
 import { NavLink} from 'react-router-dom';
+import Kely from "./kely.png";
+
+const Link = () => {
+    return(
+        <div>
+            <NavLink to="/">
+                <img src={Kely} class="titanic naked"/>
+            </NavLink>
+        </div>
+    );
+}
 
 const About = () => {
     return(
         <div id="about">
             <div className="container dad" >
                 <div className="row son" >
-                    <div className="home col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <NavLink to="/home" className="grow vertical-tab-2">
+                    <div className="home ">
+                            <div className="grow vertical-tab-2 col-lg-3 col-md-3 col-sm-3 col-xs-3">
                                 <p className="rotateAbout text-justify">About</p>
-                            </NavLink>
+                            </div>
                     </div>
+                    <Link/>
                 </div>
                 <div id="aboutme" className="row" >
-                    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9">
                             <div  className="grow">
                                 <p className="tab-about text-center">About me</p> 
-                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div className="">
                                     <h1 className="text-center"><img className="img-circle img-responsive" src ="https://raw.githubusercontent.com/KelyKley/Portafolio-FINAL/master/public/img/me.png"/></h1>
                                 </div>
-                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div className="">
                                 <p className="text-justify introduction">Soy Kely Añamuro, una desarrolladora web front-end jr.</p> 
-                                <p className="text-justify introduction">Estoy interesado en todo lo que sea web. </p>
                                 <p className="text-justify introduction">Pasé los últimos meses en el bootcamp de Laboratoria - Arequipa, donde tuve la oportunidad de proveerme 
                                 de muchos conocimientos, tiempo durante el cual diseñé
                                 y construí diversas páginas web funcionales.</p>
--- src/contact.js
+++ src/contact.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import { connect } from "redux-zero/react";
 import "./css/app.css";
 import { NavLink} from 'react-router-dom';
+import Kely from "./kely.png";
+
+const Link = () => {
+    return(
+        <div>
+            <NavLink to="/">
+                <img src={Kely} class="titanic naked"/>
+            </NavLink>
+        </div>
+    );
+}
 
 const FormContact = () => {
     return(
@@ -11,19 +22,19 @@ const FormContact = () => {
                     <form className="col-lg-8 col-md-9 col-sm-8 col-xs-9">
                         <div className="">
                             <div className="form-group">
-                                <label for="exampleInputUsername">Your name</label>
-                                <input type="text" className="form-control" id="" placeholder=" Enter Name"/>
+                                <label for="exampleInputUsername">Nombre :</label>
+                                <input type="text" className="form-control" id="" placeholder="Ingresa tu nombre"/>
                             </div>
                             <div className="form-group">
-                                <label for="exampleInputEmail">Email Address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail" placeholder="Enter email"/>
+                                <label for="exampleInputEmail">Email :</label>
+                                <input type="email" className="form-control" id="exampleInputEmail" placeholder="Ingresa tu email"/>
                             </div>	
                             
                         </div>
                         <div className="">
                             <div className="form-group">
-                                <label for ="description"> Message</label>
-                                <textarea  className="form-control" id="description" placeholder="Enter Your Message"></textarea>
+                                <label for ="description"> Mensaje :</label>
+                                <textarea  className="form-control" id="description" placeholder="Escribeme..."></textarea>
                             </div>
                             <div>
 
@@ -44,14 +55,15 @@ const Contact = () => {
             <div className="container dad" >
                 <div className="row son" >
                     <div className="home col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <NavLink to="/home" className="grow vertical-tab-4">
+                            <div className="grow vertical-tab-4">
                                 <p className="tab-title">4</p> 
                                 <p className="rotateContact rotate">Contact</p>
-                            </NavLink>
+                            </div>
                     </div>
                 </div>
+                <Link/>
                 <div id="aboutme" className="row" >
-                    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9">
                             <div  className="grow">
                             <p className="tab-about text-center">Contact me </p> 
                                 <div>
@@ -62,6 +74,7 @@ const Contact = () => {
                                     <li className="twitter"><a href="#"><i className="fa fa-file-text-o fa-3x"></i></a></li>
                                     <li className="google"><a href="#"><i className="fa fa-google-plus fa-3x"></i></a></li>
                                     <li className="linkedin"><a href="#"><i className="fa fa-linkedin fa-3x"></i></a></li>
+                                    <li className="codepen"><a href="#"><i className="fa fa-github fa-3x"></i></a></li>
                                 </ul>
                             </div>
                     </div>
